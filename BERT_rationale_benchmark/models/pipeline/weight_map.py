51b1c41acf2a3a1d5323ea8ede19ccf154e2ef04
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-11 17:12:44 +0300

    trying the old classifier

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -37,7 +37,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 directory = "C:/Users/Dor_local/Downloads/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/"
 data_dir = "C:/Users/Dor_local/Downloads/movies.tar/movies" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/movies"
 
-suffix = "_gt_aw_all"
+suffix = "_001_gt_lay.pt"
 
 best_validation_score = 0
 best_validation_epoch = 0
