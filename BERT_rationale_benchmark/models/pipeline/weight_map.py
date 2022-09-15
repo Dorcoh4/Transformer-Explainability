c877e99f2ca3c3ef6acc229d1fff258c14f76dae
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-15 20:53:53 +0300

    retro used movie data

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -38,7 +38,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 directory = "C:/Users/Dor_local/Downloads/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/"
 data_dir = "C:/Users/Dor_local/Downloads/movies.tar/movies" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/movies"
 
-suffix = "_retro_bug"
+suffix = "_retro_bug_movie"
 
 best_validation_score = 0
 best_validation_epoch = 0
@@ -427,13 +427,13 @@ def main():
     docids = set(e.docid for e in
                  chain.from_iterable(chain.from_iterable(map(lambda ann: ann.evidences, chain(train, val, test)))))
     documents = load_documents(data_dir, docids)
-    imdb_data = load_dataset("imdb")
-    print(f"IMDB dataset - train:{len(imdb_data['train'])}, test:{imdb_data['test']}")
-    # train_dataset = convert_dataset(train, documents, "train", imdb_data['train'])
-    # val_dataset = convert_dataset(val, documents, "validation")
-    # test_dataset = convert_dataset(test, documents, "test")
+    # imdb_data = load_dataset("imdb")
+    # print(f"IMDB dataset - train:{len(imdb_data['train'])}, test:{imdb_data['test']}")
+    train_dataset = convert_dataset(train, documents, "train")
+    val_dataset = convert_dataset(val, documents, "validation")
+    test_dataset = convert_dataset(test, documents, "test")
 
-    train_dataset = tokenize_dataset(imdb_data['train'])
+    train_dataset = tokenize_dataset(train_dataset)
     # val_dataset = tokenize_dataset(val_dataset)
     # test_dataset = tokenize_dataset(test_dataset)
 
