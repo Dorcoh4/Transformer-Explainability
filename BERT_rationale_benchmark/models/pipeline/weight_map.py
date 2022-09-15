0973416b006e4a975a2701ed2377d7db6b7e20cc
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-15 20:44:45 +0300

    Update weight_map.py

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -38,7 +38,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 directory = "C:/Users/Dor_local/Downloads/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/"
 data_dir = "C:/Users/Dor_local/Downloads/movies.tar/movies" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/movies"
 
-suffix = "_retro_hm"
+suffix = "_retro_bug"
 
 best_validation_score = 0
 best_validation_epoch = 0
