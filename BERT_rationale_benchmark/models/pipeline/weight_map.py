fdbb3110a873a45cd54028f7fd63557e863e02bb
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-21 17:40:55 +0300

    Update weight_map.py

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -293,20 +293,20 @@ def epoch_validation(epoch, mask_model, classifier, tokenizer,  val, word_intern
                 }
                 results.append(result_dict)
                 # result_files[res].write(json.dumps(result_dict) + "\n")
-    truth = list(chain.from_iterable(Rationale.from_annotation(ann) for ann in annotations))
-    pred = list(chain.from_iterable(Rationale.from_instance(inst) for inst in results))
-    token_level_truth = list(chain.from_iterable(rat.to_token_level() for rat in truth))
-    token_level_pred = list(chain.from_iterable(rat.to_token_level() for rat in pred))
-    token_level_prf = score_hard_rationale_predictions(token_level_truth, token_level_pred)
-    print(token_level_prf)
-    value = token_level_prf['instance_macro']['f1']
-    if value > best_validation_score:
-        best_validation_score = value
-        best_validation_epoch = epoch
-    print(f"epoch {epoch} validation score: {value}")
-    print(f"BEST epoch {best_validation_epoch} BEST score: {best_validation_score}")
-    mask_model.train()
-    return value
+                truth = list(chain.from_iterable(Rationale.from_annotation(ann) for ann in annotations))
+                pred = list(chain.from_iterable(Rationale.from_instance(inst) for inst in results))
+                token_level_truth = list(chain.from_iterable(rat.to_token_level() for rat in truth))
+                token_level_pred = list(chain.from_iterable(rat.to_token_level() for rat in pred))
+                token_level_prf = score_hard_rationale_predictions(token_level_truth, token_level_pred)
+                print(token_level_prf)
+                value = token_level_prf['instance_macro']['f1']
+        if value > best_validation_score:
+            best_validation_score = value
+            best_validation_epoch = epoch
+        print(f"epoch {epoch} validation score: {value}")
+        print(f"BEST epoch {best_validation_epoch} BEST score: {best_validation_score}")
+        mask_model.train()
+        return value
 
 
 
