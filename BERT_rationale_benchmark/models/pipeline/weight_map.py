df72df412e753ce287fb6a56ab3ee719afca7842
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-22 18:56:36 +0300

    trying to use entropy instead of regularization

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -41,7 +41,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 directory = "C:/Users/Dor_local/Downloads/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/"
 data_dir = "C:/Users/Dor_local/Downloads/movies.tar/movies/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/movies/"
 
-suffix = "_bert_d2_unfreeze"
+suffix = "_bert_ce+e"
 
 best_validation_score = 0
 best_validation_epoch = 0
@@ -157,10 +157,10 @@ def train_masker(classifier, classify_tokenizer, train_dataset, val, word_intern
     for param in classifier.parameters():
         param.requires_grad = False
 
-    # for param in mask_model.bert.parameters():
-    #     param.requires_grad = False
-    # for param in mask_model.bert.encoder.layer[11].parameters():
-    #     param.requires_grad = True
+    for param in mask_model.bert.parameters():
+        param.requires_grad = False
+    for param in mask_model.bert.encoder.layer[11].parameters():
+        param.requires_grad = True
 
     mask_model.train()
     crossEntropyLoss = torch.nn.CrossEntropyLoss()
@@ -200,14 +200,15 @@ def train_masker(classifier, classify_tokenizer, train_dataset, val, word_intern
             batch['inputs_embeds'] = masked_in
             out2 = classifier(**batch)
             # original_probs = softmax(out1.logits)
-            # masked_probs = softmax(out2.logits)
+            masked_probs = softmax(out2.logits)
 
             ce_loss = crossEntropyLoss(out2.logits, labels)
             mask_loss = torch.norm(relevant_mask, 1, dim=1).sum() / batch_size
+            entropy_loss = crossEntropyLoss(out2.logits, masked_probs)
             # mask_losses = - torch.var(mask, dim=1, unbiased=False)
             # mask_loss = mask_losses.sum() / batch_size
 
-            loss = ce_loss + lambda1 * mask_loss
+            loss = ce_loss + lambda1 * mask_loss + entropy_loss
 
             loss.backward()
 
