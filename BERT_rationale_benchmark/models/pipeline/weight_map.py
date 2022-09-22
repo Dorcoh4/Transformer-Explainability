8306f2a2d8d17807c276d12da5876a1c2b56211e
Dorcoh4 <33184521+Dorcoh4@users.noreply.github.com>
2022-09-22 16:18:47 +0300

    d 15 lr

--- BERT_rationale_benchmark/models/pipeline/weight_map.py
+++ BERT_rationale_benchmark/models/pipeline/weight_map.py
@@ -41,7 +41,7 @@ device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cp
 directory = "C:/Users/Dor_local/Downloads/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/"
 data_dir = "C:/Users/Dor_local/Downloads/movies.tar/movies/" if 'win' in sys.platform else "/home/joberant/NLP_2122/dorcoh4/weight_map/movies/"
 
-suffix = "_bert_d2_lr"
+suffix = "_bert_d15_lr"
 
 best_validation_score = 0
 best_validation_epoch = 0
@@ -171,7 +171,7 @@ def train_masker(classifier, classify_tokenizer, train_dataset, val, word_intern
 
     progress_bar = tqdm(range(num_training_steps))
     lambda1 = 0
-    output_dropout = 0.2
+    output_dropout = 0.15
     for epoch in range(num_epochs):
         running_loss = 0
         running_loss_ce = 0
